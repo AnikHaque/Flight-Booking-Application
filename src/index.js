27fab2c4c497fe78ed7cd42352261ae73749abe5
AnikHaque <ekramul15-811@diu.edu.bd>
2023-04-07 03:28:58 +0600

    added

--- src/index.js
+++ src/index.js
@@ -12,6 +12,5 @@ root.render(
   </React.StrictMode>
 );
 
-// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
